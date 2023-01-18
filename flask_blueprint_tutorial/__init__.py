23d80a3243ef163dbda8e903ab10ff062191d62f
hiven <mail@hiven.com>
2023-01-18 19:02:50 +0000

    Update __init__.py

--- flask_blueprint_tutorial/__init__.py
+++ flask_blueprint_tutorial/__init__.py
@@ -1,6 +1,5 @@
 """Initialize Flask app."""
 from flask import Flask
-from flask_assets import Environment
 
 from config import Config
 
@@ -9,12 +8,9 @@ def init_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.Config")
-    assets = Environment()
-    assets.init_app(app)
 
     with app.app_context():
         # Import parts of our application
-        from .assets import compile_static_assets
         from .home import home
         from .products import products
         from .profile import profile
@@ -23,8 +19,5 @@ def init_app():
         app.register_blueprint(profile.profile_bp)
         app.register_blueprint(home.home_bp)
         app.register_blueprint(products.product_bp)
-
-        # Compile static assets
-        compile_static_assets(assets)
 
         return app
